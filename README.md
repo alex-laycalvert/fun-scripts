18d5e1dccfa6d49605edca9e6165bff08df1081d
alex-laycalvert <alex.laycalvert@gmail.com>
2021-12-25 08:55:43 -0500

    Modified README.md and christmas.sh

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### christmas.sh
 
-This is slightly modified from [this script by sergiolepore](https://raw.githubusercontent.com/sergiolepore/ChristBASHTree/e757367e54e4d95166d9037b31211ad087e246fe/tree-EN.sh) to center the tree vertically.
+This is slightly modified from [this script by sergiolepore](https://raw.githubusercontent.com/sergiolepore/ChristBASHTree/e757367e54e4d95166d9037b31211ad087e246fe/tree-EN.sh) to center it vertically and format it a little different. Also added a Nerd Font star on top.
 
 I would tell you that this is not [this Christmas Tree](https://en.wikipedia.org/wiki/Christmas_Tree_EXEC) but would you really believe me?
--- christmas.sh
+++ christmas.sh
@@ -10,6 +10,9 @@ est=$((c-2))
 color=0
 tput setaf 2; tput bold
 
+tput cup $((lin - 1)) $col
+echo 六
+
 # Tree
 for ((i=1; i<20; i+=2))
 {
@@ -30,11 +33,15 @@ for ((i=1; i<=3; i++))
     tput cup $((lin++)) $c
     echo 'mWm'
 }
+tput cup $((lin++)) $c
+echo ""
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo MERRY CHRISTMAS
-tput cup $((lin + 1)) $((c - 10)); echo And lots of CODE in $new_year
+tput cup $lin $((c - 15)); echo "Merry Christmas you Filthy Animal!"
+tput cup $((lin + 1)) $c
+echo ""
+tput cup $((lin+ 2)) $((c - 10)); echo And lots of CODE in $new_year
 let c++
 k=1
 
@@ -61,7 +68,7 @@ while true; do
         sh=1
         for l in C O D E
         do
-            tput cup $((lin+1)) $((c+sh))
+            tput cup $((lin+2)) $((c+sh))
             echo $l
             let sh++
             sleep 0.01
